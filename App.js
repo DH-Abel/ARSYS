423321292bff0b32c3a09b0f8b07d8fc52f813b1
DH-Abel <157434617+DH-Abel@users.noreply.github.com>
2024-11-17 19:47:49 -0400

    TEST

--- App.js
+++ App.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import InicioScreen from './pages/InicioScreen';
-import Clientes from './navigation/StackNavigation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Test from './pages/test';
 
 
-
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, errorInfo) {
-    console.log('Error capturado:', error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <Text>Ocurrió un error. Revisa la consola.</Text>;
-    }
-    return this.props.children;
-  }
-}
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <ErrorBoundary>
+    <View>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="InicioScreen" component={InicioScreen} />
+        <Stack.Navigator initialRouteName="Test">
+          <Stack.Screen name="Test" component={Test} />
         </Stack.Navigator>
       </NavigationContainer>
-    </ErrorBoundary>
+        
+    </View>
   );
 }
---
+++ pages/test.js
@@ -0,0 +1,17 @@
+//Esta pestaña se encarga de la gestion de los clientes, como visualizar la lista de clientes 
+//tambien se encarga de cargar //ClienteInformacion que es el contenido dentro de la tabla
+import React, { useState } from 'react';
+import { Text, Modal, Pressable, SafeAreaView, FlatList, Alert } from 'react-native'
+import Styles from '../Components/Styles';
+
+const Test = () => {
+
+  return (
+    <SafeAreaView>
+      <Text style={Styles.titulo}>CLIENTES</Text>
+    
+
+    </SafeAreaView>
+  )
+}
+export default Test
